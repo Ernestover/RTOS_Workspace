d0ef1dc69646e9b66761a4bdfc387c0f29599d5d
Ernest Stover <ernestover123@gmail.com>
2026-03-14 22:30:26 -0500

    Initial commit

---
+++ FreeRTOS_Projects/Debug/ThirdParty/FreeRTOS/list.d
@@ -0,0 +1,17 @@
+ThirdParty/FreeRTOS/list.o: ../ThirdParty/FreeRTOS/list.c \
+ C:/Users/ernes/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/ernes/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ C:/Users/ernes/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/projdefs.h \
+ C:/Users/ernes/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/portable.h \
+ C:/Users/ernes/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/ernes/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/ernes/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/ernes/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/list.h
+C:/Users/ernes/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+C:/Users/ernes/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+C:/Users/ernes/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/projdefs.h:
+C:/Users/ernes/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/portable.h:
+C:/Users/ernes/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/ernes/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/ernes/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/ernes/Workspace/RTOS_Workspace/FreeRTOS_Projects/ThirdParty/FreeRTOS/include/list.h:
